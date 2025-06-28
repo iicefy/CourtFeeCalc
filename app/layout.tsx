7e45052d20d59797a20a47958526655cb1de41be
iicefy <chawit_rittiya@hotmail.com>
2025-06-28 15:32:40 +0700

    Fix formatting issues in layout component and improve container styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ const font = Nunito_Sans({
   weight: ["400", "600", "700"],
   style: "normal",
   preload: false,
-})
+});
 
 export const metadata: Metadata = {
   title: "CourtFeeCalc",
@@ -20,11 +20,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${font.className} antialiased`}
-      >
-        <div className="p-4 space-y-4">
-          <h1 className="text-xl font-extrabold text-green-800">CourtFeeCalc</h1>
+      <body className={`${font.className} antialiased`}>
+        <div className="p-4 space-y-4 mx-auto max-w-[500px]">
+          <h1 className="text-xl font-extrabold text-green-800">
+            CourtFeeCalc
+          </h1>
           {children}
         </div>
       </body>
